2b918b986fd30112636e0642127b609c854dbb14
Ali Mohammadi <alimohammadi123450@gmail.com>
2025-07-15 23:59:41 -0700

    token add

--- routes/web.php
+++ routes/web.php
@@ -10,5 +10,9 @@ Route::get('/', function () {
 Route::get('/register', [AuthController::class, 'showRegister'])->name('register.form');
 Route::post('/register', [AuthController::class, 'register'])->name('register.post');
 Route::get('/verify-email/{token}', [AuthController::class, 'verifyEmail'])->name('verify.email');
+
 Route::get('/login', [AuthController::class, 'showLogin'])->name('login.form');
+Route::post('/login', [AuthController::class, 'login'])->name('login.post');
+
+
 
